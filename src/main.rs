8f1867962123544f56df5a961d8430ce0167ad0b
ThyW <zir@legionnaire.local>
2021-10-22 21:39:20 +0200

    in place variable substitution, changes to derfile

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs;
 use std::io;
 use std::path;
+use std::process;
 
 type Args = Vec<Arg>;
 
@@ -50,22 +51,19 @@ impl Template {
 #[derive(Debug, Clone, Default)]
 struct Variable {
     _name: String,
-    _value: Vec<String>,
+    value: Vec<String>,
 }
 
 impl Variable {
-    fn new(_name: String, _value: Vec<String>) -> Self {
-        Self {
-            _name,
-            _value
-        }
+    fn new(_name: String, value: Vec<String>) -> Self {
+        Self { _name, value }
     }
 }
 
 #[derive(Debug, Clone, Default)]
 struct Derfile {
     templates: HashMap<String, Template>,
-    _vars: HashMap<String, Variable>,
+    vars: HashMap<String, Variable>,
 }
 
 impl Derfile {
@@ -78,11 +76,62 @@ impl Derfile {
     }
 
     fn add_var(&mut self, name: String, value: Vec<String>) {
-        self._vars.insert(name.clone(), Variable::new(name, value));
+        self.vars.insert(name.clone(), Variable::new(name, value));
     }
 
-    fn _get_var(&mut self, name: &String) -> Option<&mut Variable> {
-        self._vars.get_mut(name)
+    fn get_var(&mut self, name: &String) -> Option<&mut Variable> {
+        self.vars.get_mut(name)
+    }
+
+    fn parse(&mut self) -> Self {
+        let mut derfile: Derfile = Default::default();
+        let mut self_clone = self.clone();
+        for (template_name, template) in self.templates.iter() {
+            derfile.add_template(template_name.clone());
+            let mut temp = derfile.get_template(&template_name).unwrap();
+            temp.name = template_name.to_string();
+            if template.final_name.starts_with("$") {
+                let variable_name = template.final_name.strip_prefix("$").unwrap().to_string();
+
+                if let Some(variable) = self_clone.get_var(&variable_name) {
+                    let mut temp = derfile.get_template(&template.name).unwrap();
+                    temp.final_name = variable.value[0].clone(); // only take the fist value, sicne we only accept only one final file name
+                }
+            } else {
+                let mut temp = derfile.get_template(&template_name).unwrap();
+                temp.final_name = template.final_name.clone();
+            }
+            if template.apply_path.starts_with("$") {
+                let variable_name = template.apply_path.strip_prefix("$").unwrap().to_string();
+
+                if let Some(variable) = self_clone.get_var(&variable_name) {
+                    let mut template = derfile.get_template(&template.name).unwrap();
+                    template.apply_path = variable.value[0].clone(); // only take the first value, since we only accpet one apply path now
+                }
+            } else {
+                let mut temp = derfile.get_template(&template_name).unwrap();
+                temp.apply_path = template.apply_path.clone()
+            }
+            let mut hostname_clone: Vec<String> = Vec::new();
+            for hostname_entry in template.hostnames.iter() {
+                if hostname_entry.starts_with("$") {
+                    if let Some(variable) =
+                        self_clone.get_var(&hostname_entry.strip_prefix("$").unwrap().to_string())
+                    {
+                        let mut variable_value = variable.value.clone();
+                        hostname_clone.append(&mut variable_value);
+                    }
+                } else {
+                    hostname_clone.push(hostname_entry.to_string())
+                }
+            }
+
+            let template = derfile.get_template(&template.name).unwrap();
+            template.hostnames = hostname_clone
+        }
+        derfile.vars = self.vars.clone();
+
+        derfile
     }
 }
 
@@ -105,9 +154,42 @@ fn parse_args(args: Vec<String>) -> Args {
     ret
 }
 
+fn execute_code(command: String) -> Option<String> {
+    let output: Option<String>;
+    if command.contains(" ") {
+        let split: Vec<&str> = command.split(" ").collect();
+        let cmd = split[0];
+        let args = &split[1..];
+
+        let process = process::Command::new(cmd)
+            .args(args)
+            .output();
+        if let Ok(out) = process {
+            let str = std::str::from_utf8(&out.stdout);
+            let str = str.expect("Unable to convert command output to string");
+            output = Some(str.to_string())
+        } else {
+            output = Some("".to_string())
+        }
+    } else {
+        let process = process::Command::new(command)
+            .output();
+        if let Ok(out) = process {
+            let str = std::str::from_utf8(&out.stdout);
+            let str = str.expect("Unable to convert command output to string");
+            output = Some(str.to_string())
+        } else {
+            output = Some("".to_string())
+        }
+
+    }
+
+    output
+}
+
 // TODO:
 //  [x] templates
-//  [ ] variables
+//  [x] variables ->
 //      [ ] variable from code execution?
 fn load_derfile(path: &path::Path) -> io::Result<Derfile> {
     let buffer = fs::read_to_string(path)?;
@@ -129,12 +211,15 @@ fn load_derfile(path: &path::Path) -> io::Result<Derfile> {
         .map(|each| return each.1.to_string())
         .collect();
 
+    // TODO
+    // [ ] variables from shell code
     for line in var_lines.iter() {
         if line.contains("=") {
             let split = line.split_at(line.find("=").unwrap());
             let name = split.0.trim().strip_prefix("$").unwrap().to_string();
             let value: Vec<String>;
-            let right_side = split.1
+            let right_side = split
+                .1
                 .trim()
                 .to_string()
                 .strip_prefix("=")
@@ -143,7 +228,8 @@ fn load_derfile(path: &path::Path) -> io::Result<Derfile> {
                 .to_string();
 
             if right_side.contains(",") {
-                let split: Vec<String> = right_side.split(",")
+                let split: Vec<String> = right_side
+                    .split(",")
                     .map(|x| x.trim().to_string())
                     .collect();
                 value = split
@@ -243,7 +329,7 @@ fn load_derfile(path: &path::Path) -> io::Result<Derfile> {
                         }
                     }
                     some => {
-                        if some.starts_with("$"){
+                        if some.starts_with("$") {
                             continue;
                         } else {
                             eprintln!("WARN: {} is not a valid template filed!", split.0)
@@ -254,7 +340,7 @@ fn load_derfile(path: &path::Path) -> io::Result<Derfile> {
         }
     }
 
-    Ok(derfile)
+    Ok(derfile.parse())
 }
 
 fn run(args: Args) -> io::Result<()> {
@@ -262,7 +348,10 @@ fn run(args: Args) -> io::Result<()> {
         match arg {
             Arg::Derfile(file) => {
                 let df = load_derfile(path::Path::new(&file)).unwrap();
-                println!("{:#?}", df)
+                println!("{:#?}", df);
+                // TODO: not sure if it should work like this
+                // or if the vars substitution should be done while parsing
+                // let df = Derfile::apply(df);
             }
             // TODO: this might be useless
             Arg::Apply(_path) => {
